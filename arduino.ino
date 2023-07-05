87130e8329f7345d0e7d0ee55dc22eafed376ed0
sanae42 <61277158+sanae42@users.noreply.github.com>
2023-07-06 00:17:11 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,23 +1,59 @@
+#include "DHT.h"
+
+//HC-SR04 Ultrasonic Distance Sensor
 #define PIN_TRIG 3
 #define PIN_ECHO 2
+//LED
+#define PIN_LED 4
+//DHT
+#define PIN_DHT 5     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11
+DHT dht(PIN_DHT, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+  //HC-SR04 Ultrasonic Distance Sensor
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
+    //LED
+  pinMode(PIN_LED, OUTPUT);
+  //DHT
+  dht.begin();
 }
 
 void loop() {
-  // Start a new measurement:
-  digitalWrite(PIN_TRIG, HIGH);
+  //HC-SR04 Ultrasonic Distance Sensor
+  // digitalWrite(trigPin, LOW); // Clears the trigPin condition
+  // delayMicroseconds(2);
+  digitalWrite(PIN_TRIG, HIGH);// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG, LOW);
-
-  // Read the result:
-  int duration = pulseIn(PIN_ECHO, HIGH);
+  long duration; // duration of sound wave travel
+  int distance; // distance measurement
+  duration = pulseIn(PIN_ECHO, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance = duration * 0.034 / 2; // Calculating the distance. Speed of sound wave divided by 2 (go and back)
   Serial.print("Distance in CM: ");
-  Serial.println(duration / 58);
+  Serial.print(distance );
 
+  float h = dht.readHumidity();//读湿度
+  float t = dht.readTemperature();//读温度(摄氏度)
 
-  delay(1000);
+  Serial.print("Humidity:");
+  Serial.print(h);
+  Serial.print("% Temperature:");
+  Serial.print(t);
+  Serial.println("℃");
+
+  // blink();
+
+  delay(3000);
+}
+
+// LED blink function
+// blink函数有点问题
+void blink() {
+  digitalWrite(PIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       
+  digitalWrite(PIN_LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       
 }
