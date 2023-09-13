1f742dbf71eb273f9c6f872bb001b5c08d765281
sanae42 <61277158+sanae42@users.noreply.github.com>
2023-09-13 15:49:26 +0800

    Update esp8266.ino
    Improved code annotations

--- esp8266.ino
+++ esp8266.ino
@@ -1,24 +1,28 @@
-/**
-* refer to : https://blog.csdn.net/m0_52991090/article/details/122157194
+/*
+* File: esp8266.ino
+* Author: P Geng
+* Refer to : https://blog.csdn.net/m0_52991090/article/details/122157194
+* Date: 2023-09-13
+* Description: Program for ESP8266 Board
+* Version: 1.0
 */
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include <SoftwareSerial.h> // 引入软串口库
 
-char P_NAME[] = "OnePlus";           //设置热点名称
-char P_PSWD[] = "1234567890";          //设置热点密码
-char sub[] = "TrashCanSub/1";    //设置设备SUB名称
-char pub[] = "TrashCanPub";    //设置设备PUB名称
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // MQTT related library
+#include <SoftwareSerial.h> // Soft serial port library
+
+char P_NAME[] = "OnePlus";           //WIFI name
+char P_PSWD[] = "1234567890";          //WIFI password
+char sub[] = "TrashCanSub/1";    //Subscribed topic
+char pub[] = "TrashCanPub";    //Published topic
 int id = 1;
  
 const char *ssid = P_NAME;
 const char *password = P_PSWD;
-const char *mqtt_server = "47.98.247.122";
+const char *mqtt_server = "47.98.247.122"; //IP address of server
 String reStr;
 WiFiClient espClient;
 PubSubClient client(espClient);
-// #define MSG_BUFFER_SIZE (50)
-// char msg[MSG_BUFFER_SIZE];
 
 #define D5 (14)
 #define D6 (12)
@@ -52,14 +56,12 @@ void reconnect()
   {
     String clientId = "ESP8266Client";
     clientId += String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str()))
+    if (client.connect(clientId.c_str())) //Successfully connected
     {
-      // client.publish(pub, "{\"State\":\"OnLine\"}"); //连接成功后发消息：{"State":"OnLine"}
       client.subscribe(sub);
     }
     else
     {
-      // Serial.print(client.state());
       delay(5000);
     }
   }
@@ -67,13 +69,11 @@ void reconnect()
  
 void setup()
 {
-  // pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(9600);
   mqttSerial.begin(9600, SWSERIAL_8N1, D5, D6, false, 128);
   setup_wifi();
-  client.setServer(mqtt_server, 1883); //1883端口
+  client.setServer(mqtt_server, 1883); //1883 port
   client.setCallback(callback);
-  // digitalWrite(BUILTIN_LED, HIGH);
 }
  
 void loop()
@@ -85,7 +85,6 @@ void loop()
   }
   client.loop();
 
-  // delay(3000); //delay会造成字符错位
   if (mqttSerial.available() > 0)
   {
     reStr = mqttSerial.readStringUntil('\n');
